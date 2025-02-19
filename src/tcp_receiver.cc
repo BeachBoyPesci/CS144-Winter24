47dfc8b1c4f1ec9d08ee4a4ef9bfaafa53f978d3
Pesci <mrsmallfat@163.com>
2025-02-19 19:41:23 +0800

    lab2 done(learning)

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -2,6 +2,35 @@
 
 using namespace std;
 
-void TCPReceiver::receive( TCPSenderMessage message ) {}
+void TCPReceiver::receive( TCPSenderMessage message )
+{
+  const uint64_t checkpoint = reassembler_.writer().bytes_pushed() + isn_.has_value();
+  if ( message.RST ) {
+    reassembler_.reader().set_error();
+  } else if ( checkpoint > 0 && checkpoint <= UINT32_MAX && message.seqno == isn_ )
+    return; // 拦截非法的序列号
+  if ( !isn_.has_value() ) {
+    if ( !message.SYN )
+      return;
+    isn_ = message.seqno;
+  }
+  const uint64_t abso_seqno_ = message.seqno.unwrap( *isn_, checkpoint );
+  reassembler_.insert( abso_seqno_ == 0 ? abso_seqno_ : abso_seqno_ - 1, move( message.payload ), message.FIN );
+}
 
-TCPReceiverMessage TCPReceiver::send() const {}
+TCPReceiverMessage TCPReceiver::send() const
+{
+  TCPReceiverMessage msg;
+  if ( isn_.has_value() ) {
+    const uint64_t next_byte = reassembler_.writer().bytes_pushed() + 1;
+    const uint64_t abso_ackno = next_byte + reassembler_.writer().is_closed();
+    msg.ackno = Wrap32::wrap( abso_ackno, *isn_ );
+  }
+
+  const uint16_t wnd_size = static_cast<uint16_t>(
+    min( reassembler_.writer().available_capacity(), static_cast<uint64_t>( UINT16_MAX ) ) );
+  msg.window_size = wnd_size;
+
+  msg.RST = reassembler_.reader().has_error();
+  return msg;
+}
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -2,6 +2,18 @@
 
 using namespace std;
 
-Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point ) {}
+Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
+{
+  return Wrap32 { zero_point.raw_value_ + static_cast<uint32_t>( n ) };
+}
 
-uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const {}
+uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
+{
+  constexpr uint64_t upper = static_cast<uint64_t>( UINT32_MAX ) + 1;
+  uint32_t ckpt_mod = wrap( checkpoint, zero_point ).raw_value_; // 将checkpoint转化为32位
+  uint32_t dif = raw_value_ - ckpt_mod;                          // 求出偏移量
+  if ( dif <= ( upper >> 1 ) || checkpoint + dif < upper )
+    // 如果偏移量小于2^31，或者checkpoint太小使得checkpoint左边不可取得（因为至少需要减去2^32）
+    return checkpoint + dif;
+  return checkpoint + dif - upper;
+}
