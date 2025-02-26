2b15d19cd173b0c70669123dfd7bda10933f3d62
Pesci <mrsmallfat@163.com>
2025-02-26 20:26:48 +0800

    Wrap done

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,15 +4,16 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  return Wrap32 { zero_point.raw_value_ + static_cast<uint32_t>( n ) };
+  return Wrap32 { static_cast<uint32_t>( n ) + zero_point.raw_value_ };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   const uint64_t upper = static_cast<uint64_t>( UINT32_MAX ) + 1;
-  uint32_t ck_mod = Wrap32::wrap( checkpoint, zero_point ).raw_value_; // 将checkpoint映射到uint32区间内
-  uint64_t dis = raw_value_ - ck_mod;
-  if ( dis < ( upper >> 1 ) || checkpoint + dis < upper ) // 如果dis小于2^15或者答案小于2^16，就不需要减去2^16
+  const uint32_t ck_mod
+    = Wrap32::wrap( checkpoint, zero_point ).raw_value_; // 将checkpoint映射到32位数，便于计算dis
+  uint32_t dis = raw_value_ - ck_mod;
+  if ( dis < ( upper >> 1 ) || checkpoint + dis < upper ) // 如果dis<2^15或者checkpoint太小
     return checkpoint + dis;
-  return checkpoint - upper + dis;
+  return checkpoint + dis - upper;
 }
