723aa8eae4d46b7158e1e9f2e8addc06f4090690
BeachBoyPesci <mrsmallfat@163.com>
2025-07-10 16:55:52 +0800

    lab2 done

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -2,35 +2,33 @@
 
 using namespace std;
 
-// 需要注意，SYN，FIN，RST为F时，不占用空间，这虽然不符合struct的知识，但是这道题目似乎就是这个意思，否则不会设置一个sequence_length()函数。这个事情只能解释为这个课程做了一些简化，实际的生产代码里面，估计SYN标志位除了第一次以外，都不占字节
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  if ( writer().has_error() )
-    return;
   if ( message.RST ) {
     reader().set_error();
     return;
   }
+  if ( message.seqno == ISN_ ) { // 非法序列号
+    return;
+  }
+  uint64_t check_point = writer().bytes_pushed() + ISN_.has_value();
   if ( !ISN_.has_value() ) {
     if ( !message.SYN )
       return;
-    ISN_.emplace( message.seqno );
+    ISN_ = message.seqno;
   }
-  Wrap32 zero_point = ISN_.value();
-  uint64_t checkpoint = writer().bytes_pushed() + 1; // checkpoint就是正在期待的下一个字节序号
-  uint64_t abso_seqno = message.seqno.unwrap( zero_point, checkpoint );
-  uint64_t stream_index = abso_seqno + message.SYN - 1;
-  // 如果SYN为T，说明它是第一次传输，那么abso_seqno为0；如果SYN为F，说明是正常传输，要减去SYN标志位所占用的一个字节
-  reassembler_.insert( stream_index, move( message.payload ), message.FIN );
+  uint64_t abso_sqno = message.seqno.unwrap( *ISN_, check_point );
+  reassembler_.insert( abso_sqno == 0 ? 0 : abso_sqno - 1, move( message.payload ), message.FIN );
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  uint16_t wd_size
-    = static_cast<uint16_t>( min( writer().available_capacity(), static_cast<uint64_t>( UINT16_MAX ) ) );
+  TCPReceiverMessage message;
+  uint64_t check_point = writer().bytes_pushed() + ISN_.has_value();
   if ( ISN_.has_value() ) {
-    Wrap32 ackno = Wrap32::wrap( writer().bytes_pushed() + writer().is_closed(), ISN_.value() ) + 1;
-    return { ackno, wd_size, writer().has_error() };
+    message.ackno = Wrap32::wrap( check_point + writer().is_closed(), *ISN_ );
   }
-  return { nullopt, wd_size, writer().has_error() };
+  message.window_size = min( writer().available_capacity(), static_cast<uint64_t>( UINT16_MAX ) );
+  message.RST = reader().has_error();
+  return message;
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,18 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  return Wrap32 { static_cast<uint32_t>( n ) + zero_point.raw_value_ };
+  return zero_point + static_cast<uint32_t>( n );
 }
 
+// zero_point是一层包装
+// 计算出最终的偏移量，然后加在checkpoint上
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  const uint64_t upper = static_cast<uint64_t>( UINT32_MAX ) + 1;
-  const uint32_t ck_mod
-    = Wrap32::wrap( checkpoint, zero_point ).raw_value_; // 将checkpoint映射到32位数，便于计算dis
-  uint32_t dis = raw_value_ - ck_mod;
-  if ( dis < ( upper >> 1 ) || checkpoint + dis < upper ) // 如果dis<2^15或者checkpoint太小
-    return checkpoint + dis;
-  return checkpoint + dis - upper;
+  uint64_t upper = static_cast<uint64_t>( UINT32_MAX ) + 1;
+  uint32_t ckpt_mod = Wrap32::wrap( checkpoint, zero_point ).raw_value_;
+  uint32_t distance = raw_value_ - ckpt_mod;
+  if ( distance <= ( upper >> 1 )
+       || checkpoint + distance < upper ) // distabnce小于等于2^31，或者checkpoint + distance太小，无法减去upper
+    return checkpoint + distance;
+  return checkpoint + distance - upper;
 }
